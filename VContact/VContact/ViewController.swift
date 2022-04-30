00c7a199c2e31a051642756185fa52a43e3ab4fb
Shendrikov <shendricov@yandex.ru>
2022-04-30 19:25:36 +1000

    Lesson 1 for GeekBrains

---
+++ VContact/VContact/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VContact
+//
+//  Created by Wally on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+
+
+}
+
